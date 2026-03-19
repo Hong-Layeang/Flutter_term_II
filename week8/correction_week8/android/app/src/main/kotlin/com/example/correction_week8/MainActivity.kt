279ab998cbdbd4bd3370fc085eece33b0a0378bd
Hong-Layeang <layeang.hong@student.cadt.edu.kh>
2026-03-19 16:03:03 +0700

    correction week8

---
+++ week8/correction_week8/android/app/src/main/kotlin/com/example/correction_week8/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.correction_week8
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ week8/correction_week8/lib/test/song_repository_test.dart
@@ -0,0 +1,24 @@
+import '../data/repositories/songs/song_repository_mock.dart';
+import '../data/repositories/songs/song_repository.dart';
+import '../model/songs/song.dart';
+
+void main() async {
+  //   Instantiate the  song_repository_mock
+  SongRepository songRepository = SongRepositoryMock();
+
+  // Test both the success and the failure of the post request
+  Song? song = await songRepository.fetchSongById("s1");
+  print("Song received ${song!.title}");
+
+  try {
+    Song? song = await songRepository.fetchSongById("s9");
+    print("Song received ${song!.title}");
+
+  } catch (e) {
+    print(e);
+  }
+
+  // Handle the Future using 2 ways  (2 tests)
+  // - Using then() with .catchError().
+  // - Using async/await with try/catch.
+}
